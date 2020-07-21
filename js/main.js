b9ba12b631a9a0afc9d0cc1989b2b8294437a8cb
Daniel Andrade <danny83910@gmail.com>
2020-07-21 15:14:19 -0300

    Novo site Dunamis

---
+++ js/main.js
@@ -0,0 +1,41 @@
+(function(win, doc) {
+    'Use strict';
+
+    $(document).ready(function() {
+        /*PARALLAX */
+        $('.parallax').parallax();
+        /*SIDENAV */
+        $(".button-collapse").sideNav();
+        /*SLIDER */
+        $('.slider').slider({
+            indicators: false,
+            interval: 5000
+        });
+        /*CARROSEL*/
+        $('.carousel').carousel();
+        /*FUNCTION NEXT CARROSEL*/
+        $('.next').click(function() {
+            $('.carousel').carousel('next')
+        });
+        /*FUNCTION PREV CARROSEL*/
+        $('.prev').click(function() {
+            $('.carousel').carousel('prev')
+        });
+
+        /*MATRIAL BOX*/
+        $('.materialboxed').materialbox();
+    });
+
+    //Função para mudar a cor da nav, de tranparente para azul
+    $(document).ready(function() {
+        $(window).scroll(function() {
+            if ($(window).scrollTop() > 10) {
+                $('.menu').addClass('grey lighten-1');
+            } else {
+                $('.menu').removeClass('grey lighten-1');
+            }
+
+        })
+    });
+
+})(window, document);
